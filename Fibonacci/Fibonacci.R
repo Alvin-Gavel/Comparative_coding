e539fcda09c8c6d80bb6fcc96967ae14e7ee1078
Alvin-Gavel <alvin.gavel@gmail.com>
2022-12-05 11:46:48 +0100

    Implemented Fibonacci in R

---
+++ Fibonacci/Fibonacci.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+fibonacci <- function(n) {
+  F_n <- 1
+  F_nm1 <- 0
+  for (i in 1:n) {
+    F_old <- F_n
+    F_n <- F_n + F_nm1
+    F_nm1 <- F_old
+  }
+  cat(F_nm1, '\n')
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+n <- args[1]
+fibonacci(n)
