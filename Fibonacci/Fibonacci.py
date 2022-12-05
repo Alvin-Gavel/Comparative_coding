3e4c4dbe639405f9b1b7d0ebc15d9c0fd092c9dd
Alvin-Gavel <alvin.gavel@gmail.com>
2022-12-05 11:38:07 +0100

    Implemented fibonacci in Python 3

--- Fibonacci/Fibonacci.py
+++ Fibonacci/Fibonacci.py
@@ -3,6 +3,13 @@
 import math
 import sys
 
-n = sys.argv
+def fibonacci(n):
+   F_n = 1
+   F_nm1 = 0
+   for i in range(n):
+     F_n, F_nm1 = F_n + F_nm1, F_n
+   print(F_nm1)
+   return
 
-print(n)
+n = int(sys.argv[1])
+fibonacci(n)
