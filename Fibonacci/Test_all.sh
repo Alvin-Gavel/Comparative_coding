cf33cd46a9d6c623641c5e35d6c84c2d6eadfa71
Alvin-Gavel <alvin.gavel@gmail.com>
2023-01-05 10:08:44 +0100

    Compressed code into associative arrays

--- Fibonacci/Test_all.sh
+++ Fibonacci/Test_all.sh
@@ -2,10 +2,9 @@
 
 # Left to my own devices I would have used ls here, so thanks to this guy for deterring me: https://stackoverflow.com/a/938052/11890275
 
-py_regex='.*\.py'
-R_regex='.*\.R'
-php_regex='.*\.php'
-sh_regex='.*\.sh'
+languages=('python' 'R' 'php' 'bash')
+declare -A regexen=( ['python']='.*\.py' ['R']='.*\.R' ['php']='.*\.php' ['bash']='.*\.sh' )
+declare -A run_commands=( ['python']='python3' ['R']='Rscript' ['php']='php' ['bash']='bash' )
 
 file_regex='Fibonacci\..*'
 
@@ -15,18 +14,13 @@ for filename in $(find .)
 do
   if [[ $filename =~ $file_regex ]]
   then
-     if [[ $filename =~ $py_regex ]]
-     then
-         python3 $filename $n
-     elif [[ $filename =~ $R_regex ]]
-     then
-         Rscript $filename $n
-     elif [[ $filename =~ $php_regex ]]
-     then
-         php $filename $n
-     elif [[ $filename =~ $sh_regex ]]
-     then
-         sh $filename $n
-     fi
+    for language in ${languages[@]}
+    do
+      if [[ $filename =~ ${regexen[$language]} ]]
+      then
+        echo $"Testing $language script:"
+        ${run_commands[$language]} $filename $n
+      fi
+    done
   fi
 done
