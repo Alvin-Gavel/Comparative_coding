4ad55d6b8194e9837f503bbb6bb47e88e6370f4f
Alvin-Gavel <alvin.gavel@gmail.com>
2023-01-05 10:24:02 +0100

    Abstracted out generic testing function, added test to Primality_test.foo as well.

--- Primality_test/Test_all.sh
+++ Primality_test/Test_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 path=$(pwd)
-file_regex='Fibonacci\..*'
-n=10
+file_regex='Primality_test\..*'
+n=13
 
 bash ../Utilities/Test_contents_of_directory.sh $path $file_regex $n
---
+++ Utilities/Test_contents_of_directory.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Left to my own devices I would have used ls here, so thanks to this guy for deterring me: https://stackoverflow.com/a/938052/11890275
+
+test_all() {
+  directory_path=$1
+  file_regex=$2
+  argument=$3
+
+  languages=('python' 'R' 'php' 'bash')
+  declare -A regexen=( ['python']='.*\.py' ['R']='.*\.R' ['php']='.*\.php' ['bash']='.*\.sh' )
+  declare -A run_commands=( ['python']='python3' ['R']='Rscript' ['php']='php' ['bash']='bash' )
+
+  for filename in $(find $directory_path)
+  do
+    if [[ $filename =~ $file_regex ]]
+    then
+      for language in ${languages[@]}
+      do
+        if [[ $filename =~ ${regexen[$language]} ]]
+        then
+          echo $"Testing $language script:"
+          ${run_commands[$language]} $filename $argument
+        fi
+      done
+    fi
+  done
+}
+
+test_all $1 $2 $3
