714b22f9e02938519f1999136b1f9ae271a18f92
Alvin-Gavel <alvin.gavel@gmail.com>
2023-01-05 18:08:16 +0100

    Made indentation consistent

--- Primality_test/Primality_test.php
+++ Primality_test/Primality_test.php
@@ -1,21 +1,21 @@
 <?php
 
 function isprime($n) {
-    $found_divisor = false;
-    if ($n <= 1) {
-        return false;
-    } else {
-        for ($i = 2; $i <= sqrt($n); $i++) {
-            $found_divisor = ($found_divisor or $n % $i == 0);
-        }
-        return !$found_divisor;
-    }
+   $found_divisor = false;
+   if ($n <= 1) {
+      return false;
+   } else {
+      for ($i = 2; $i <= sqrt($n); $i++) {
+         $found_divisor = ($found_divisor or $n % $i == 0);
+      }
+      return !$found_divisor;
+   }
 }
 
 function verbose_answer($n) {
-    $wasit = isprime($n);
-    $wasit ? $str_not = '' : $str_not = ' not';
-    echo "$n is$str_not prime\n";
+   $wasit = isprime($n);
+   $wasit ? $str_not = '' : $str_not = ' not';
+   echo "$n is$str_not prime\n";
 }
 
 verbose_answer($argv[1]);
