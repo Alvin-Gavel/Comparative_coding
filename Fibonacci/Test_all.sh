7fa39dbb9caea3ecc2fafa5c0ca4c20c4dea082d
Alvin-Gavel <alvin.gavel@gmail.com>
2023-01-05 09:29:36 +0100

    Automatic identification of relevant code files

--- Fibonacci/Test_all.sh
+++ Fibonacci/Test_all.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
-python3 Fibonacci.py 5
-Rscript Fibonacci.R 5
-php Fibonacci.php 5
-sh Fibonacci.sh 5
+# Left to my own devices I would have used ls here, so thanks to this guy for deterring me: https://stackoverflow.com/a/938052/11890275
+
+py_regex='.*\.py'
+R_regex='.*\.R'
+php_regex='.*\.php'
+sh_regex='.*\.sh'
+
+file_regex='Fibonacci\..*'
+
+n=10
+
+for filename in $(find .)
+do
+  if [[ $filename =~ $file_regex ]]
+  then
+     if [[ $filename =~ $py_regex ]]
+     then
+         python3 $filename $n
+     elif [[ $filename =~ $R_regex ]]
+     then
+         Rscript $filename $n
+     elif [[ $filename =~ $php_regex ]]
+     then
+         php $filename $n
+     elif [[ $filename =~ $sh_regex ]]
+     then
+         sh $filename $n
+     fi
+  fi
+done
